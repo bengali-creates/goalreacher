bb028ae1d00643498822e9111d3d989aed758ae2
bengali-creates <debasish2005pal@gmail.com>
2026-02-02 02:59:35 +0530

    feat: linked the quize to the interview tab

--- src/app/(main)/interview/page.tsx
+++ src/app/(main)/interview/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
-const InterviewPage = () => {
+import StatsCards from './_components/stats-card';
+import PerformanceChart from './_components/performance-chart';
+import QuizList from './_components/quiz-list';
+import { getAssessments } from 'actions/interview';
+const InterviewPage = async() => {
+const assessments=await getAssessments()
   return (
-    <div>InterviewPage</div>
-  )
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
 }
 
 export default InterviewPage
